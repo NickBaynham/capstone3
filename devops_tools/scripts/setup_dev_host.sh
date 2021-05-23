2010ae04d4431b30064e70d095fc05e5812d9197
NickBaynham <nickbaynham@gmail.com>
2021-05-23 19:20:20 +0000

    added a setup script for setting up a develpment host

---
+++ devops_tools/scripts/setup_dev_host.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Usage: setup_dev_host.sh  <github user name> <github email>"
+  exit 1
+fi
+
+USER_NAME=$1
+EMAIL=$2
+
+echo "Setting up a dev host for $USER_NAME with email address of $EMAIL"
+
+sudo apt-get update
+sudo apt-get install -y tree
+
+# Install AWS CLI
+apt install -y unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
+rm awscliv2.zip
+rm -r ./aws
+
+# Install Terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt install terraform
+terraform --version
+
+# Clone the 'capstone3' repository
+git --version
+config --global user.email "$EMAIL"
+config --global user.name "$USER_NAME"
+git clone https://github.com/NickBaynham/capstone3.git
+tree capstone3
+echo To configure AWS run 'aws configure'
+
