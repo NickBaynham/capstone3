acccd117548b753d8bd672da99bbf1c2bc274794
NickBaynham <nickbaynham@gmail.com>
2021-05-29 18:04:08 +0000

    fixed an issue with infra cicd instance.tf

--- infrastructure/cicd/instance.tf
+++ infrastructure/cicd/instance.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "cicd-server" {
-  ami           = var.CICD_IMAGE
-  instance_type = var.CICD_SHAPE
-  subnet_id     = aws_subnet.cicd-subnet-public.id
-  key_name      = aws_key_pair.cicd-key-pair.key_name
+  ami                    = var.CICD_IMAGE
+  instance_type          = var.CICD_SHAPE
+  subnet_id              = aws_subnet.cicd-subnet-public.id
+  key_name               = aws_key_pair.cicd-key-pair.key_name
+  vpc_security_group_ids = [aws_security_group.ssh-allowed.id]
 
   tags = {
     Name = "cicd-server"
