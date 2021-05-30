4fa2d5cc6333d1598e537d1743ed0e1b3e5ae403
nickbaynham <nickbaynham@gmail.com>
2021-05-30 15:08:59 +0000

    Added a script to setup ansible host (removed user setup from test/instance.tf)

--- infrastructure/test/instance.tf
+++ infrastructure/test/instance.tf
@@ -5,20 +5,6 @@ resource "aws_instance" "master-node" {
   vpc_security_group_ids = [aws_security_group.ssh-allowed.id]
   key_name               = aws_key_pair.cluster-key-pair.id
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo useradd -m -s /bin/bash ansible",
-      "sudo su ansible",
-      "sudo touch /home/ansible/.ssh/authorized_keys",
-      "sudo echo file(var.PUBLIC_KEY_PATH) > authorized_keys",
-      "sudo mv authorized_keys /home/ansible/.ssh/authorized_keys",
-      "sudo chown -R ansible:ansible /home/ansible/.ssh",
-      "sudo chmod 700 /home/ansible/.ssh",
-      "sudo chmod 600 /home/ansible/.ssh/authorized_keys",
-      "sudo usermod -aG sudo ansible"
-    ]
-  }
-
   connection {
     user        = var.EC2_USER
     private_key = file(var.PRIVATE_KEY_PATH)
---
+++ scripts/add-user.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## Run as Root: sudo su -
+
+if [ $# -ne 2 ]; then
+  echo "Usage: add-user.sh <user_name> <public_key>"
+  exit 1
+fi
+
+export USER_NAME="$1"
+export PUBLIC_KEY="$2"
+
+adduser --disabled-password --gecos '' $USER_NAME
+mkdir -p /home/${USER_NAME}/.ssh
+touch /home/${USER_NAME}/.ssh/authorized_keys
+echo $PUBLIC_KEY > authorized_keys
+mv authorized_keys /home/${USER_NAME}/.ssh
+chown -R ${USER_NAME}:${USER_NAME} /home/${USER_NAME}/.ssh
+chmod 0700 /home/${USER_NAME}/.ssh
+chmod 0600 /home/${USER_NAME}/.ssh/authorized_keys
+usermod -aG sudo $USER_NAME
+touch /etc/sudoers.d/${USER_NAME}
+chmod 0600 /etc/sudoers.d/${USER_NAME}
+echo "${USER_NAME} 	ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/${USER_NAME}
