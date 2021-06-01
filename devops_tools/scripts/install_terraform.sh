f4e5db69a5909c6e6adaaf9d91e0a9ba598e8478
nickbaynham <nickbaynham@gmail.com>
2021-06-01 22:31:08 +0000

    added steps to install_terraform

--- devops_tools/scripts/install_terraform.sh
+++ devops_tools/scripts/install_terraform.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo apt update
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-apt install terraform
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt update
+sudo apt-get install -y terraform
 terraform --version
