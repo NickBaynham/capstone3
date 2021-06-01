39edaa0a6c7801430434a93918a4ad7c5ec952f2
nickbaynham <nickbaynham@gmail.com>
2021-06-01 22:47:04 +0000

    updated install_aws_cli

--- devops_tools/scripts/install_aws_cli.sh
+++ devops_tools/scripts/install_aws_cli.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
+sudo apt update
 sudo apt install -y unzip
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
+sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo unzip awscliv2.zip
+sudo ./aws/install
+rm -r aws/
 rm awscliv2.zip
 aws --version
+
