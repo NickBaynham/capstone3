862994347434f5912175237a376de704f57eddb7
nickbaynham <nickbaynham@gmail.com>
2021-06-01 23:34:22 +0000

    fixes to install_ansible script

---
+++ devops_tools/scripts/install_ansible.sh
@@ -0,0 +1,16 @@
+sudo apt update
+sudo apt install -y ansible
+
+# Install boto3 and boto-core
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository -y universe
+sudo apt-get update
+sudo apt-get install -y python3-pip
+pip3 install boto3
+
+# Install Ansible AWS EC2 Inventory Source
+ansible-galaxy collection install amazon.aws
+
+#cp /etc/ansible/ansible.cfg /etc/ansible/ansible.cfg.bak
+#cp ~/capstone3/devops_tools/config/ansible.cfg /etc/ansible/ansible.cfg
+ansible --version
