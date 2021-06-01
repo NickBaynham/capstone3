201f7f5af9b2b9e9d942db644cf247d55170421b
NickBaynham <nickbaynham@gmail.com>
2021-06-01 00:02:15 +0000

    changed the application (cicd continuous eval)

--- application/src/main/java/com/github/nickbaynham/captstone3/lib/TimeStamp.java
+++ application/src/main/java/com/github/nickbaynham/captstone3/lib/TimeStamp.java
@@ -1,10 +1,12 @@
 package com.github.nickbaynham.captstone3.lib;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class TimeStamp {
     public static String getTime() {
         LocalDate localDate = LocalDate.now();
-        return localDate.toString();
+        LocalTime now = LocalTime.now();
+	return localDate.toString() + now.toString();
     }
 }
