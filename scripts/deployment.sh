754e798dc3543636ba50d9e81417ee0ba26a2f21
Nick Baynham <nickbaynham@gmail.com>
2021-06-01 17:22:28 -0400

    Update deployment.sh

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-kubectl apply -f https://raw.githubusercontent.com/NickBaynham/capstone3/main/deployment/backend-deployment.yml
-kubectl apply -f https://raw.githubusercontent.com/NickBaynham/capstone3/main/deployment/backend-service.yml
-kubectl apply -f https://raw.githubusercontent.com/NickBaynham/capstone3/main/deployment/frontend-deployment.yml
-kubectl apply -f https://raw.githubusercontent.com/NickBaynham/capstone3/main/deployment/frontend-service.yml
-kubectl apply -f https://raw.githubusercontent.com/NickBaynham/capstone3/main/deployment/app-deployment.yml
-
+export IMAGE=$1
+kubectl set image deployment time-app time-app=$IMAGE
